134f5691b30ffe854a87b6b476f8d8efab86db76
RemyC <remycottrez@gmail.com>
2022-05-20 14:17:05 +0200

    services : structure ok, reste json à faire et à importer + cartes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Home } from './pages/Home';
-import { Asso } from './pages/Asso';
+import { Services } from './pages/Services';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Asso />    
+    <Services />    
   </React.StrictMode>
 );
---
+++ src/pages/Services.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../css/club.css';
+
+export const Services = () => {
+    return (
+        <div className="services">
+            <header className="services_header">
+                <h1>Nos services</h1>
+            </header>
+            <article>
+                <section className="services_presentation">
+                    <h2>Présentation</h2>
+                    <p>Lorem ipsum dolor sit amet. Ut possimus omnis in saepe consectetur ut quidem galisum est alias. Est quos voluptatum ut assumenda velit et optio aspernatur sed temporibus ipsum. Aut voluptatem molestias aut temporibus Quis et aliquam temporibus. Et perspiciatis odit et voluptatem fugit et sint galisum ut quia nihil aut iste minus et nobis eligendi sed tempora beatae? Est vero minus vel sint enim est maxime nisi id officia officia sit accusantium placeat in voluptates voluptatem quo veritatis ipsa.</p>
+                </section>
+                <hr></hr>
+                <section className="services_locations">
+                    <h2>Nos produits à louer</h2>
+                </section>
+            </article>
+        </div>
+    )
+}
