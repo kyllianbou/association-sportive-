8692f6cd8dc62c79567f980e22930a05a8ba540d
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-20 11:26:43 +0200

    ok

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/contact.css';
+
+const contact = () =>{
+return(
+  <div className='headder' >
+<h1>Contact</h1>
+</div>
+);
+}
+  
+ 
+  
+  export default contact
