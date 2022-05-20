06a853ced90fac57e968f7d8025e34b91841c572
RemyC <remycottrez@gmail.com>
2022-05-20 12:46:39 +0200

    asso crée

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Home } from './pages/Home';
+import { Asso } from './pages/Asso';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />    
+    <Asso />    
   </React.StrictMode>
 );
