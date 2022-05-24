08d3a83484b633049743edb230c0bf87c7b6a29f
ThibautPoinloup <poinloupthibaut@gmail.com>
2022-05-24 09:42:08 +0200

    creation login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,26 +8,19 @@ const Login = () => {
     return (
         <div>
             <Nav />
-           <div className="inscription">
-             <article>
-                 <input type="e-mail" placeholder='adresse@mail.com'/>
-                 <input type="password" placeholder='Mot de passe'/>
-                 <button>Connexion</button>
-                 <i class="fa-brands fa-facebook"></i>
-                 <i class="fa-brands fa-twitter"></i>
-                 <i class="fa-brands fa-instagram"></i>
-                 <i class="fa-brands fa-tiktok"></i>
-             </article>
-             <article>
-                 <input type="e-mail" placeholder='adresse@mail.com'/>
-                 <input type="password" placeholder='Mot de passe'/>
-                 <button>Connexion</button>
-                 <i class="fa-brands fa-facebook"></i>
-                 <i class="fa-brands fa-twitter"></i>
-                 <i class="fa-brands fa-instagram"></i>
-                 <i class="fa-brands fa-tiktok"></i>
-             </article>
-           </div>
+          <div className="box">
+              <article>
+                  <input type="email" placeholder='adresse@mail.com' />
+                  <input type="password" placeholder='Mot de passe' />
+                  <button>Connexion</button>
+                  <div className="resaux-sociaux">
+                  <i class="fa-brands fa-facebook-f"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-instagram"></i>
+                  <i class="fa-brands fa-tiktok"></i>
+                  </div>
+              </article>
+          </div>
         </div>
     );
 };
