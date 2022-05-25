3ff50720b34c61ad1ae061c510e14d32143ae2ce
ThibautPoinloup <poinloupthibaut@gmail.com>
2022-05-25 11:48:47 +0200

    fin de la page profil

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profil from './pages/Profil';
-import Services from './pages/Services';
+// import Services from './pages/Services';
 const App = () => {
     return (
         <div>
@@ -13,7 +13,7 @@ const App = () => {
                     <Route path='/Home' element={<Home/>}></Route>
                     <Route path='/Login' element={<Login/>}></Route>
                     <Route path='/Profil' element={<Profil/>}></Route>
-                    <Route path='/Services' element={<Services/>}></Route>
+                    {/* <Route path='/Services' element={<Services/>}></Route> */}
                 </Routes>
             </BrowserRouter>
         </div>
--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -8,18 +8,18 @@ const Profil = () => {
             <Nav/>
             <div className="box-profil">
                 <article className='name'>
-                    <a href='https://fr-fr.facebook.com/'><div className="avatar"></div></a>
+                    <a href=''><div className="avatar"></div></a>
                     <h2>Jean-Didier</h2>
                     <h4>année de création : 2 juin 2020</h4>
                     <button className="deconnexion">DECONNEXION</button>
                 </article>
-                <span className="trait"></span>
+                <span className="trait-profil"></span>
                 <article className='preference'>
-                    <a href='https://fr-fr.facebook.com/'>Mon panier</a>
-                    <a href='https://fr-fr.facebook.com/'>Pagaies couleurs : Pagaie Noir M/V/C</a>
-                    <a href='https://fr-fr.facebook.com/'>Année de Kayak : 10 ans</a>
-                    <a href='https://fr-fr.facebook.com/'>Article préféré : Stand up paddle</a>
-                    <a href='https://fr-fr.facebook.com/'>plat favoris : Hamburger</a>
+                    <a href=''>Mon panier</a>
+                    <a href=''>Pagaies couleurs : Pagaie Noir M/V/C</a>
+                    <a href=''>Année de Kayak : 10 ans</a>
+                    <a href=''>Article préféré : Stand up paddle</a>
+                    <a href=''>plat favoris : Hamburger</a>
                 </article>
             </div>
         </div>
