94107e6b4183afbcbfb3b1347e70f8d758e372ac
Eddydebuisson <eddydebuisson@gmail.com>
2022-05-25 10:09:22 +0200

    merge eddy to dev

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Club from './components/Club';
 import App from './App';
 import './index.css';
-import Nav from './components/Nav';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
