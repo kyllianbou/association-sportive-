49c3d8259597599f36fcd29c3c7a81f44851a44a
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-25 10:06:51 +0200

    contact

---
+++ src/pages/Profil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import '../css/profil.css';
+
+const Profil = () => {
+    return (
+        <div className='profil'>
+            <Nav/>
+        </div>
+    );
+};
+
+export default Profil;
