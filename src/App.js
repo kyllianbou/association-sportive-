12682a30daae3a0c11fd2b3c96f1bcb30fbf967c
ThibautPoinloup <poinloupthibaut@gmail.com>
2022-05-25 16:16:03 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Slide from './pages/Slide';
 import Club from './components/Club';
 import Login from './pages/Login';
 import Profil from './pages/Profil';
-// import Services from './pages/Services';
+import Services from './pages/Services';
 const App = () => {
     return (
         <> 
@@ -14,7 +14,7 @@ const App = () => {
                     <Route path='/Club' element={<Club/>}/>
                     <Route path='/Login' element={<Login/>}></Route>
                     <Route path='/Profil' element={<Profil/>}></Route>
-                    {/* <Route path='/Services' element={<Services/>}></Route> */}
+                    <Route path='/Services' element={<Services/>}></Route>
                 </Routes>
         </Router>
         </>
