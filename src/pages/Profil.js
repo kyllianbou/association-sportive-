80bc9342156da512bf386a980659136c79aa5bcc
ThibautPoinloup <poinloupthibaut@gmail.com>
2022-05-25 14:40:42 +0200

    reccuperation de la branche a eddy et suppression de contact

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -8,7 +8,7 @@ const Profil = () => {
             <Nav/>
             <div className="box-profil">
                 <article className='name'>
-                    <a href=''><div className="avatar"></div></a>
+                    <a href=''><div className="avatar"><img src="../logo192.png" alt="" /></div></a>
                     <h2>Jean-Didier</h2>
                     <h4>année de création : 2 juin 2020</h4>
                     <button className="deconnexion">DECONNEXION</button>
