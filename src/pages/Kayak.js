e3c2a05f0e550ee56cb88f41257dca62fe787d0e
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-25 16:11:38 +0200

    polo

--- src/pages/Kayak.js
+++ src/pages/Kayak.js
@@ -5,16 +5,16 @@ import Nav from '../components/Nav'
 
 const Contact = () => {
     return (
-        <><div className='headder'>
+        <><div className='headde'>
             <Nav />
             <h1>Kayak Polo</h1>
-        </div><article className='toto' >
+        </div><article id='toto' >
                 <h1>Presentation</h1>
-                <div className='texte' >
+                <div id='texte' >
                 <p>Lorem ipsum dolor sit amet,g nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendreritveuvgueihfiowhfuwchjsauycmndsavucgdsvgcjhgsuhfvea8o</p></div>
-                <h1 className='color' >Equipes Hommes Du Club</h1> 
+                <h1 id='color' >Equipes Hommes Du Club</h1> 
                 <img src='images/Homme.png' alt='photo de kayak' />
-            <h1 className='color' >Equipes Femmes Du Club</h1> 
+            <h1 id='color' >Equipes Femmes Du Club</h1> 
             <img src='images/Femme.png' alt='photo de kayak' /></article>
         </>
     );
