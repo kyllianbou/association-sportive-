79d7e859f3bde7ee5edc05e762def6e47c38ccac
Eddydebuisson <eddydebuisson@gmail.com>
2022-05-26 13:34:52 +0200

    rediction du bouton en-savoir-plus

--- src/components/Club.js
+++ src/components/Club.js
@@ -26,9 +26,9 @@ const Club = () => {
                                 <img src="./images/kaya-img.png" alt='kayak'></img>
                             </div>
                             <div className="grid__item d-flex">
-                                <h2>ECOLE DE PAGAIE</h2>
+                                <h2>École de paggaie</h2>
                                 <p>Lorem ipsum dolor sit amet,um ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit </p>
-                                <a href='#aaa'>En savoir plus</a>
+                                <a href='/Ecole'>En savoir plus</a>
                             </div>
                         </div>
                     </div>
@@ -42,7 +42,7 @@ const Club = () => {
                             <div className="grid__item d-flex right">
                                 <h2>Kayak polo</h2>
                                 <p>Lorem ipsum dolor sit amet,s elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit </p>
-                                <a href='#aaa'>En savoir plus</a>
+                                <a href='/Kayak'>En savoir plus</a>
                             </div>
                         </div>
                     </div>
@@ -54,9 +54,9 @@ const Club = () => {
                                 <img src="./images/kaya-img.png" alt='kayak'></img>
                             </div>
                             <div className="grid__item d-flex">
-                                <h2>SERVICES</h2>
+                                <h2>Services</h2>
                                 <p>Lorem ipsum dolor sortor,  dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit </p>
-                                <a href='#aaa'>En savoir plus</a>
+                                <a href='/Services'>En savoir plus</a>
                             </div>
                         </div>
                     </div>
