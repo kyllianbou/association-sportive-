aea52d99bc39322a0bd4813512ca511827304e76
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-27 11:48:17 +0200

    ok

---
+++ src/pages/Gallerie.js
@@ -0,0 +1,23 @@
+import "../css/Gallerie.css";
+import Nav from "../components/Nav";
+import Footer from "./Footer";
+
+const Club = () => {
+
+    return (
+        <>
+            <Nav />
+            <div className="body">
+          
+               
+                
+            </div>
+            <Footer />
+        </>
+    );
+
+
+
+};
+
+export default Club;
