4ea8eda9257eef18c97b88465bf72fc306f914b7
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-30 11:58:31 +0200

    galerie

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -12,15 +12,16 @@ const Galerie = () => {
           <h1>presentation de la Famille..</h1>
         </div>
             <div className="bod">
-            <img src='images/Homme.png' alt='photo de kayak' />
-            <img src='images/lesputes.jpeg' alt='photo de kayak' />
-            <img src='images/salope.jpeg' alt='photo de kayak' />
-            <img src='images/images.jpeg' alt='photo de kayak' />
-            <img src='images/chienne.jpeg' alt='photo de kayak' />
             <img src='images/thibault.jpg' alt='photo de kayak' />
-            <img src='images/Homme.png' alt='photo de kayak' />
-            <img src='images/femme.png' alt='photo de kayak' />
-            <img src='images/Homme.png' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
+            <img src='images/thibault.jpg' alt='photo de kayak' />
             </div>
             <Footer />
         </>
