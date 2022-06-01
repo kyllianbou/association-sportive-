ac2144a6ba2ee00a2dc64aede779d237921b92a9
Your Name <you@example.com>
2022-06-01 09:49:59 +0200

    ok

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,50 +1,97 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import '../css/nav.css';
-import { slide as Menu } from 'react-burger-menu';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "../css/nav.css";
+import { slide as Menu } from "react-burger-menu";
 
 const Nav = () => {
-    const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-    const changeBackground = () => {
-        if (window.scrollY >= 500) {
-            setNavbar(true);
-        } else {
-            setNavbar(false);
-        }
-    };
+  const changeBackground = () => {
+    if (window.scrollY >= 500) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
 
-    window.addEventListener('scroll', changeBackground);
+  window.addEventListener("scroll", changeBackground);
 
-
-    return (
-        <>
-            <nav className={navbar ? 'navbar active' : 'navbar'}>
-                <NavLink to="/" className='brand'>ckcg avion</NavLink>
-                <ul className='nav-links'>
-                    <li><NavLink to="/" className='link-item'>accueil</NavLink></li>
-                    <li><NavLink to="/Club" className='link-item'>association</NavLink></li>
-                    <li><NavLink to="/Services" className='link-item'>services</NavLink></li>
-                    <li><NavLink to="/Galerie" className='link-item'>galerie</NavLink></li>
-                    <li><NavLink to="/Contact" className='link-item'>contact</NavLink></li>
-                </ul>
-                <NavLink to="/Login" className='connexion'>connexion</NavLink>
-            </nav>
-            <Menu className='burger'>
-                <nav>
-                    <NavLink to="/Home" className='brand'>ckcg avion</NavLink>
-                    <ul className='nav-links'>
-                        <li><NavLink to="/Home" className='link-item'>accueil</NavLink></li>
-                        <li><NavLink to="/Club" className='link-item'>association</NavLink></li>
-                        <li><NavLink to="/Services" className='link-item'>services</NavLink></li>
-                        <li><NavLink to="/Galerie" className='link-item'>gallerie</NavLink></li>
-                        <li><NavLink to="/Contact" className='link-item'>contact</NavLink></li>
-                    </ul>
-                    <NavLink to="/Login" className='connexion'>login/sign in</NavLink>
-                </nav>
-            </Menu>
-        </>
-    );
+  return (
+    <>
+      <nav className={navbar ? "navbar active" : "navbar"}>
+        <NavLink to="/" className="brand">
+          ckcg avion
+        </NavLink>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/" className="link-item">
+              accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Club" className="link-item">
+              association
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Services" className="link-item">
+              services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Galerie" className="link-item">
+              galerie
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact" className="link-item">
+              contact
+            </NavLink>
+          </li>
+        </ul>
+        <NavLink to="/Login" className="connexion">
+          connexion
+        </NavLink>
+      </nav>
+      <Menu className="burger">
+        <nav>
+          <NavLink to="/Home" className="brand">
+            ckcg avion
+          </NavLink>
+          <ul className="nav-links">
+            <li>
+              <NavLink to="/Home" className="link-item">
+                accueil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Club" className="link-item">
+                association
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Services" className="link-item">
+                services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Galerie" className="link-item">
+                gallerie
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Contact" className="link-item">
+                contact
+              </NavLink>
+            </li>
+          </ul>
+          <NavLink to="/Login" className="connexion">
+            login/sign in
+          </NavLink>
+        </nav>
+      </Menu>
+    </>
+  );
 };
 
 export default Nav;
