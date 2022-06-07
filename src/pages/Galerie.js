4b15ff23c6007af0f366b7b743844689f8bf38ed
RemyC <remycottrez@gmail.com>
2022-06-07 12:39:55 +0200

    user-select none sur les numbers du slide

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -8,27 +8,27 @@ const Galerie = () => {
 
     ) {
         var img = document.getElementById('thibault');
-     img.style.visibility = 'visible';
+        img.style.visibility = 'visible';
     }
     return (
         <>
             <Nav />
             <div className='head'>
-          <h1>Galerie</h1>
-        </div>
+                <h1>Galerie</h1>
+            </div>
             <div className="bod">
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <img src='images/thibault.jpg' alt='Equipe de kayak' />
-            <button id="easter" onClick={setImageVisible}>c'est moi Thibault</button>
-            <img  id='thibault' src='images/thibault_leboss.jpg' alt='thibault'/>
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <img src='images/thibault.jpg' alt='Equipe de kayak' />
+                <button id="easter" onClick={setImageVisible}>c'est moi Thibault</button>
+                <img id='thibault' src='images/thibault_leboss.jpg' alt='thibault' />
             </div>
             <Footer />
         </>
