561fada2baf374ccd0cbdb000fdce4eea145ee95
RemyC <remycottrez@gmail.com>
2022-06-07 12:36:04 +0200

    HTML > JSX

--- src/pages/Club.js
+++ src/pages/Club.js
@@ -46,7 +46,7 @@ const Club = () => {
                                 <h2>Kayak polo</h2>
                                 <p>Lorem ipsum dolor sit amet,s elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit </p>
                                 <NavLink to="/Kayak" className='link-item'>
-                                    <button class='btn_clair'>En savoir plus</button>
+                                    <button className='btn_clair'>En savoir plus</button>
                                 </NavLink>
                             </div>
                         </div>
